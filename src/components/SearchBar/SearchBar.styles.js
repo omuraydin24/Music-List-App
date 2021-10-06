d2c031963dad3d795951ef53fdace989d3c8e773
omur aydin <ommuraydin@gmail.com>
2021-10-07 01:29:43 +0300

    Initial Commit

---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: '#edeff1',
+        margin: 5,
+        borderRadius: 5,
+    },
+});
